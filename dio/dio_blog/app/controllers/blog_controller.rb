bd58284f47a527f830cd463bb0d76003e90c55fd
danielfransa <daniel_fransa@hotmail.com>
2024-08-01 20:44:29 -0300

    Dio_blog

---
+++ dio/dio_blog/app/controllers/blog_controller.rb
@@ -0,0 +1,7 @@
+class BlogController < ApplicationController
+  layout "application_blank"
+
+  def index
+    @post = Post.where("published_date <= ?", Time.zone.now).order("published_date desc").first 
+  end
+end
